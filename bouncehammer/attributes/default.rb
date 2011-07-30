48e0ccdb48a5b11c4ad73ced6c338e710c4e30fa
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2011-07-30 17:47:00 +0900

    bouncehammer の属性追加とレシピの更新
    バージョンやインストール先は、アドリビュートで設定できるようにした。
    また、パッケージを積極的に利用することにした。

---
+++ bouncehammer/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:bouncehammer][:version] = "2.7.4"
+
+default[:bouncehammer][:dir] = "/usr/local/bouncehammer"
--- bouncehammer/recipes/default.rb
+++ bouncehammer/recipes/default.rb
@@ -18,34 +18,51 @@
 #
 include_recipe "perl"
 
-package "libpath-class-perl" do
-  action :install
+base_pkgs = ["build-essential", "perl-doc"]
+base_pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+perl_pkgs = [
+    "libcompress-zlib-perl",
+    "libcrypt-cbc-perl", 
+    "libcrypt-des-perl",
+    "liberror-perl",
+    "libpath-class-perl",
+    "libperl6-slurp-perl",
+    "libterm-progressbar-perl",
+    "libtext-asciitable-perl",
+    "libdbd-sqlite3-perl",
+    "libtime-piece-perl",
+    "libyaml-syck-perl"
+  ]
+perl_pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
 cpan_mods = [
   "Class::Accessor::Fast::XS",
-  "Crypt:CBC",
-  "Crypt::DES",
   "DBIx::Skinny",
   "Email::AddressParser",
-  "Error",
-  "JSON::Syck",
   "Path::Class::File::Lockable",
-  "Perl6::Slurp",
-  "Term::ProgressBar",
-  "Text::ASCIITable"]
+  ]
 cpan_mods.each do |module_name|
   cpan_module module_name 
 end
 
-version = "2.7.3"
+version = node[:bouncehammer][:version]
+dist_dir = node[:bouncehammer][:dir]
 
 remote_file "#{Chef::Config[:file_cache_path]}/bouncehammer-#{version}.tar.gz" do
   source "http://dist.bouncehammer.jp/bouncehammer-#{version}.tar.gz"
   mode "0644"
 end
 
-configure_options = "--disable-webui --prefix=/usr/local/bouncehammer"
+configure_options = "--disable-webui --prefix=#{dist_dir}"
 script "build_bouncehammer" do
   interpreter "bash"
   cwd Chef::Config[:file_cache_path]
@@ -57,7 +74,7 @@ script "build_bouncehammer" do
 end
 
 # For sqlite.
-sqlite_pkgs = ["sqlite3", "libdbd-sqlite3-perl"]
+sqlite_pkgs = ["sqlite3", ]
 sqlite_pkgs.each do |pkg|
   package pkg do
     action :install
@@ -65,7 +82,7 @@ sqlite_pkgs.each do |pkg|
 end
 script "setup_sqlite" do
   interpreter "bash"
-  cwd "/usr/local/bouncehammer"
+  cwd dist_dir
   code <<-EOF
   touch var/db/bouncehammer.db
   cat share/script/SQLite*.sql | sqlite3 var/db/bouncehammer.db
@@ -73,6 +90,6 @@ script "setup_sqlite" do
   EOF
 end
 
-cookbook_file "/usr/local/bouncehammer/etc/bouncehammer.cf" do
+cookbook_file "#{dist_dir}/etc/bouncehammer.cf" do
   source "bouncehammer.cf"
 end
