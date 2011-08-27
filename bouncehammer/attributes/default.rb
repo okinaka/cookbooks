dd2f81e6197ca9f7f8ce16e8b24750eeec10a3dc
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2011-08-27 18:38:15 +0900

    bouncehammer バージョン 2.7.5 に設定

--- bouncehammer/attributes/default.rb
+++ bouncehammer/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:bouncehammer][:version] = "2.7.4"
+default[:bouncehammer][:version] = "2.7.5"
 
 default[:bouncehammer][:dir] = "/usr/local/bouncehammer"
 default[:bouncehammer][:api] = false
