04ea1b3a95bce85ecbffbaa9a1530c703e116d8f
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2011-11-05 19:22:42 +0900

    php-fastcgi を追加

---
+++ php-fastcgi/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "OKINAKA Kenshin"
+maintainer_email "okinakak@yahoo.co.jp"
+license          "All rights reserved"
+description      "Installs/Configures php-fastcgi"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ php-fastcgi/recipes/default.rb
@@ -0,0 +1,27 @@
+%w{psmisc php5-cgi spawn-fcgi}.each do |pkg|
+  package pkg
+end
+
+template "/usr/bin/php-fastcgi" do
+  source "bin/php-fastcgi.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+execute "bootstrap" do
+  command "update-rc.d php-fastcgi defaults"
+  action :nothing
+end
+
+template "/etc/init.d/php-fastcgi" do
+  source "init.d/php-fastcgi.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  notifies :run, resources(:execute => "bootstrap")
+end
+
+service "php-fastcgi" do
+  action :restart
+end
